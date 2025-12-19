10669e66aa1bc7deb4e45e3bf13d93a461547d4c
vophong90 <141991252+vophong90@users.noreply.github.com>
2025-12-19 20:11:50 +0700

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: { persistSession: true, autoRefreshToken: true }
-  }
-);
+"use client";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+export const supabase =
+  createBrowserSupabaseClient();
