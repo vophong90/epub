9b142e106ab5fd20643570f041b64ad04ef8cb4e
vophong90 <141991252+vophong90@users.noreply.github.com>
2025-12-19 20:57:50 +0700

    Update page.tsx

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -1,4 +1,12 @@
-export default function BookDebugPage({ params }: { params: { id: string } }) {
+// app/books/[id]/page.tsx
+
+type BookPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default function BookPage({ params }: BookPageProps) {
   return (
     <main style={{ padding: 40 }}>
       <h1>BOOK-DEBUG-PAGE</h1>
